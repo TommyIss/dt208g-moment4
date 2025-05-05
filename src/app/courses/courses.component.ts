53184454de0b56474f0d19075e735c5548f144d9
Tommy Issa <tommyissa@hotmail.com>
2025-05-05 15:22:39 +0200

    Har skapat innehåll för sökfältet, samt kurstabell i html-fil, har skapat metoder i TypeScript-fil som hämtar data och lägga dem tabellform, hitta kurser genom inmatningsfält, och organisera kurser via kurskod eller kursnamn

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,69 @@
 import { Component } from '@angular/core';
+import { Course } from '../models/course';
+import { CoursesService } from '../services/courses.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
+  // Properties
+  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  searchPhrase: string = '';
+  codeOrder: string = 'desc';
+  nameOrder: string = 'desc';
+  
+  
+  constructor(private coursesService: CoursesService) {}
 
+  // Metoder
+  ngOnInit() {
+    this.coursesService.getCourses().subscribe((courses) => {
+      this.courses = courses;
+      this.filteredCourses = courses;
+    })
+  }
+
+  // Hitta kurs via namn eller kod
+  findCourse():void {
+    this.filteredCourses = this.courses.filter((course) =>
+      course.code.toLowerCase().includes(this.searchPhrase.toLowerCase()) ||
+      course.coursename.toLowerCase().includes(this.searchPhrase.toLowerCase())
+    );
+  }
+  
+  // Sortera via namn
+  sortByName(): void {
+    if(this.nameOrder === 'desc') {
+      this.nameOrder = 'asc';
+      this.filteredCourses.sort((a, b) => 
+        (a.coursename > b.coursename) ? 1 : -1
+      );
+    } else if(this.nameOrder === 'asc') {
+      this.nameOrder = 'desc';
+      this.filteredCourses.sort((a, b) => 
+      (b.coursename > a.coursename) ? 1 : -1
+      );
+    }
+  }
+
+  // Sortera via kod
+  sortByCode(): void {
+    if(this.codeOrder === 'desc') {
+      this.codeOrder = 'asc';
+      this.filteredCourses.sort((a, b) => 
+      (a.code > b.code) ? 1 : -1
+      );
+    } else if(this.codeOrder === 'asc') {
+      this.codeOrder = 'desc';
+      this.filteredCourses.sort((a, b) => 
+      (b.code > a.code) ? 1 : -1
+      );
+    }
+  }
 }
